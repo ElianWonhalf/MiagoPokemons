61e46d3e44409f80a6400b83d1a85e0b472700ce
ElianWonhalf <elianwonhalf@gmail.com>
2016-08-14 20:32:13 +0200

    Fixed lightbox

--- js/main.js
+++ js/main.js
@@ -5,46 +5,30 @@ addListener(document, 'DOMContentLoaded', function (event) {
     var lightboxContainer = document.getElementById('jslghtbx'),
         startX,
         startY,
-        dist,
-        threshold = 150, //required min distance traveled to be considered swipe
-        allowedTime = 200, // maximum time allowed to travel that distance
-        elapsedTime,
-        startTime;
+        threshold = 150;
 
     function lightboxContainerSwipeHandler(isRightSwipe) {
         if (isRightSwipe) {
-            lightbox.next();
-        } else {
             lightbox.prev();
+        } else {
+            lightbox.next();
         }
     }
 
     lightboxContainer.addEventListener('touchstart', function (e) {
         var touchObj = e.changedTouches[0];
 
-        dist = 0;
         startX = touchObj.pageX;
         startY = touchObj.pageY;
-        startTime = new Date().getTime();
-
-        e.preventDefault();
-    }, false);
-
-    lightboxContainer.addEventListener('touchmove', function (e) {
-        e.preventDefault();
     }, false);
 
     lightboxContainer.addEventListener('touchend', function (e) {
         var touchObj = e.changedTouches[0];
+        var rightDist = Math.abs(touchObj.pageX - startX) + Math.abs(touchObj.pageY - startY) >= threshold;
 
-        dist = touchObj.pageX - startX;
-        elapsedTime = new Date().getTime() - startTime;
-
-        var rightTime = elapsedTime <= allowedTime;
-        var rightDist = dist >= threshold;
-        var isRightSwipe = rightTime && rightDist && Math.abs(touchObj.pageY - startY) <= 100;
-        lightboxContainerSwipeHandler(isRightSwipe);
-
-        e.preventDefault();
+        if (rightDist) {
+            var isRightSwipe = touchObj.pageX > startX;
+            lightboxContainerSwipeHandler(isRightSwipe);
+        }
     }, false);
 });
